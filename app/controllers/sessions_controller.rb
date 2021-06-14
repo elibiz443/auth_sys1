e94861d5fafcf22701050e50c3de2d1e08632cdd
elibiz443 <ellyambet10@gmail.com>
2021-06-14 04:39:44 +0300

    work on the create and destroy methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,25 @@ class SessionsController < ApplicationController
 	end
 
 	def create
-	end
+    reset_session
+    @user = User.find_by(email: session_params[:email])
 
+    if @user && @user.authenticate(session_params[:password])
+      session[:user_id] = @user.id
+      flash[:success] = 'Welcome back!'
+      redirect_to root_path
+    else
+      flash[:error] = 'Invalid email/password combination'
+      redirect_to login_path
+    end
+  end
+
+  skip_before_action :require_valid_user!, except: [:destroy]
 	def destroy
-	end
+    reset_session
+  end
+
+  def session_params
+    params.require(:session).permit(:email, :password)
+  end
 end
