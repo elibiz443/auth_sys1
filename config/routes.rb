0a522769e28296c1d83e1476ec3a40205d3a060a
elibiz443 <ellyambet10@gmail.com>
2021-06-14 04:34:33 +0300

    Added users controller and modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+	get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :sessions, only: [:create]
+  resources :users, only: [:new, :create]
+  
 	root 'dashboard#show'
 end
